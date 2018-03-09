ee49ae53bf5660f25d34cbe4cb03e9cde72cd568
sugang <1548592920@qq.com>
2018-03-09 14:14:35 +0800

    基础类修改

--- vender/hdsSoft/BaseClass.php
+++ vender/hdsSoft/BaseClass.php
@@ -5,5 +5,37 @@ namespace hdsSoft;
 
 class BaseClass
 {
+    /**
+     * TODO
+     * @param $name
+     * @return mixed
+     */
+    public function __get($name)
+    {
+        return $this->$name;
+    }
 
+    /**
+     * TODO
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    /**
+     * @param $object对象
+     * @param $properties属性
+     * @return mixed
+     */
+    public static function configure($object, $properties)
+    {
+        foreach ($properties as $name => $value) {
+            $object->$name = $value;
+        }
+
+        return $object;
+    }
 }
